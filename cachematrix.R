1bc607d7939772496cf7f7e5802e7c664174b74b
Jonathan Meyer <jon@gisjedi.com>
2015-04-25 18:02:22 -0400

    Solution for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Allows for the caching of a matrix and its inverse
+  #
+  # Args:
+  #   x: The matrix that will cached
+  #
+  # Returns:
+  #   a list of functions defined below:
+  #   set: setter for original matrix
+  #   get: getter for cached matrix
+  #   setinverse: setter for inverted matrix
+  #   getinverse: getter for cached inverted matrix, may be NULL
+  #
+  
+  # Set NULL default for inverse prior to caching
+  inverse <- NULL
+  
+  # Define getter/setter for original matrix
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() { x }
+  # Define getter/setter for inverse matrix
+  setinverse <- function(inv) { inverse <<- inv }
+  getinverse <- function() { inverse }
+  
+  # Wire up the getters/setters for function
+  list(set = set, get = get,
+       setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Performs the inversion of a matrix, using cache if available
+  #
+  # Args:
+  #   x: The matrix that will be inverted
+  #
+  # Returns:
+  #   The input matrix x inverted
+  #
+
+  # Check for cached inverse
+  inverse = x$getinverse()
+  
+  # If already solved, return without recalculation
+  if (!is.null(inverse)) {
+    message("getting cached inverse")
+    return(inverse)
+  }
+  
+  # Only will arrive here if the cache isn't populated
+  unsolved = x$get()
+  
+  # Compute inverse and set in cache
+  inverse = solve(unsolved, ...)
+  x$setinverse(inverse)
+  
+  # Return the inverted matrix solution
+  inverse
 }
